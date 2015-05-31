8c8bfe623d6902f384614e6cc80c08036c186f94
Sebazzz <triadsebas@gmail.com>
2015-05-31 11:57:05 +0200

    Remove unused method

--- BfCompiler/Core/Visitor/SyntaxNodeVisitor.cs
+++ BfCompiler/Core/Visitor/SyntaxNodeVisitor.cs
@@ -1,12 +1,3 @@
 ﻿namespace BfCompiler.Core.Visitor {
-    using System.Collections.Generic;
-    using Syntax;
-
-    internal abstract partial class SyntaxNodeVisitor {
-        public void Visit(IEnumerable<SyntaxNode> nodes) {
-            foreach (SyntaxNode syntaxNode in nodes) {
-                this.Visit(syntaxNode);
-            }
-        }
-    }
+    internal abstract partial class SyntaxNodeVisitor {}
 }
